832f510ee18de57c787135aac81f05f54532381b
chnbsdan <chnbsdan@gmail.com>
2026-05-13 18:17:38 +0800

    Update profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "Firefly",
+	name: "史先森",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "Hello, 小史先生.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
